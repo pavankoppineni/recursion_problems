af062692da5591fe8d57520d8e9971955c49c12c
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-06-04 00:21:58 +0530

    Added string permutations problem

---
+++ src/RecursionProblems/StringPermutations/StringPermutationsV2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursionProblems.StringPermutations
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+    /// </summary>
+    public class StringPermutationsV2
+    {
+        public IList<string> GeneratePermutations(string str)
+        {
+            var permutations = new List<string>();
+            GeneratePermutations(str, 0, str.Length - 1, permutations);
+            return permutations;
+        }
+
+        private void GeneratePermutations(string str, int currentIndex, int lastIndex, IList<string> permutations)
+        {
+            if (currentIndex == lastIndex)
+            {
+                permutations.Add(str);
+                return;
+            }
+
+            for (var i = currentIndex; i <= lastIndex; i++)
+            {
+                str = Swap(str, currentIndex, i);
+                GeneratePermutations(str, currentIndex + 1, lastIndex, permutations);
+                str = Swap(str, currentIndex, i);
+            }
+        }
+
+        private string Swap(string str, int i, int j)
+        {
+            var charArray = str.ToCharArray();
+            var temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            return new string(charArray);
+        }
+    }
+}
---
+++ test/RecursionProblems.Tests/StringPermutations/StringPermutationsV3Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecursionProblems.StringPermutations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursionProblems.Tests.StringPermutations
+{
+    [TestClass]
+    public class StringPermutationsV3Tests
+    {
+        [TestMethod]
+        public void GivenString_GeneratePermutations_ReturnsListOfPermutations()
+        {
+            //Arrange
+            var str = "aab";
+            var expectedPermutations = new HashSet<string>
+            {
+                "abc",
+                "acb",
+                "bac",
+                "bca",
+                "cba",
+                "cab"
+            };
+            var stringPermutation = new StringPermutationsV3();
+
+            //Act
+            var actualPermutations = stringPermutation.GeneratePermutations(str);
+
+            //Assert
+            Assert.AreEqual(expectedPermutations.Count, actualPermutations.Count);
+            for (var i = 0; i < actualPermutations.Count; i++)
+            {
+                Assert.AreEqual(true, expectedPermutations.Contains(actualPermutations[i]));
+            }
+        }
+    }
+}
